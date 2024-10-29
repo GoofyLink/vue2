51a1e7b8cb79701e1d3757bfcc18c3e17a40c5c3
goofy <2273100559@qq.com>
2024-10-30 03:09:37 +0800

    加载高德地图

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import './plugin/aMap';
+// 初始化高德地图的 key 和插件
+// window.VueAMap.initAMapApiLoader({
+//   key: '\tb8016bb1491ae0f96725638b48623166',
+// });
 
 Vue.config.productionTip = false;
 
---
+++ src/views/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="amap-wrap">
+    <el-amap
+        vid="amapDemo"
+        :zoom="zoom"
+        :center="center"
+        class="amap-demo">
+    </el-amap>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data(){
+    return {
+      zoom: 12, // 地图放大比例
+      center: [121.469897,31.24609],  //默认地图中心 经纬度
+    }
+  }
+};
+</script>
+<style scoped lang="scss">
+.amap-wrap{
+  height: 50vh;
+}
+</style>
